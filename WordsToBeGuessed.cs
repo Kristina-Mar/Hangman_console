7d22fe72545999b739dd7eae6b5afb1c82d98307
Kristina Martiskova <kris.martiskova@gmail.com>
2024-02-02 19:44:28 +0100

    Added a new word category.

--- WordsToBeGuessed.cs
+++ WordsToBeGuessed.cs
@@ -12,11 +12,12 @@ namespace Hangman_console
         public string Category = string.Empty;
         private string[] animals = { "ORANGUTAN", "ELEPHANT", "CROCODILE", "PLATYPUS", "BUTTERFLY", "GIRAFFE", "ANTEATER", "PENGUIN", "ANGLERFISH", "SEAHORSE", "CHEETAH", "MOSQUITO", "ALPACA", "CHICKEN", "BUFFALO", "PEACOCK" };
         private string[] countries = { "TAJIKISTAN", "PARAGUAY", "BULGARIA", "INDONESIA", "LITHUANIA", "ARGENTINA", "MONGOLIA", "AZERBAIJAN", "SINGAPORE", "BAHRAIN", "LESOTHO", "MOZAMBIQUE", "TANZANIA", "ETHIOPIA", "BOTSWANA", "GUATEMALA" };
+        private string[] fruitNVeg = { "BROCCOLI", "CAULIFLOWER", "ZUCCHINI", "WATERMELON", "RASPBERRY", "LYCHEE", "GRAPEFRUIT", "PARSNIP", "ARTICHOKE", "BEETROOT", "SPINACH", "NECTARINE", "WATERCRESS", "¨BLACKCURRANT", "PASSIONFRUIT", "JACKFRUIT" };
         public string ChosenWord = string.Empty;
         private Random randomNumberGenerator = new Random();
         public void PickAWord()
         {
-            string[][] allWords = { animals, countries };
+            string[][] allWords = { animals, countries, fruitNVeg };
             int arrayNumber = randomNumberGenerator.Next(allWords.Length);
             int wordNumber = randomNumberGenerator.Next(allWords[arrayNumber].Length);
             ChosenWord = allWords[arrayNumber][wordNumber];
@@ -26,6 +27,9 @@ namespace Hangman_console
                     break;
                 case 1:
                     Category = "country";
+                    break;
+                case 2:
+                    Category = "fruit or vegetable";
                     break;
                 default:
                     break;
